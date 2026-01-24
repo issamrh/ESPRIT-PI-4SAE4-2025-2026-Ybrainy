908df1dc2034009c0348634788582441ab9a5692
issamrh <riahi.issameddine@esprit.tn>
2026-01-24 14:26:19 +0100

    added angular template+routes+configuration

---
+++ angular/angular-app/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./frontoffice/frontoffice.module').then(m => m.FrontofficeModule)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
