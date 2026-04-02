f32d779726abeb27c6ca6aee4edd9e1dd37548dc
Jacem Jouili <jacem.jouili@esprit.tn>
2026-04-02 03:01:00 +0100

    Ajout du EurekaServer

---
+++ EurekaServer_YBrainy/src/test/java/tn/esprit/eurekaserver_ybrainy/EurekaServerYBrainyApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.eurekaserver_ybrainy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerYBrainyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
