4f4648c14760976d7bd0389baf3808d8351cb721
Mohamed aziz selmi <mohamedaziz.selmi@esprit.tn>
2026-05-06 08:22:41 +0100

    fix(course): add missing MediaType import in GlobalExceptionHandler

--- PIDEV-YBrainy-E-leraning-certifications-Platform/YBRAINY/Course/tp-foyer/src/main/java/tn/esprit/tpfoyer/Exception/GlobalExceptionHandler.java
+++ PIDEV-YBrainy-E-leraning-certifications-Platform/YBRAINY/Course/tp-foyer/src/main/java/tn/esprit/tpfoyer/Exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package tn.esprit.tpfoyer.Exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
